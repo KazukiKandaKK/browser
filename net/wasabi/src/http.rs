899d18798b6431a027d949297fd7e2fc2d92e0ed
KazukiKanda <kazuki.kanda05570@gmail.com>
2024-12-01 09:35:53 +0900

    a

--- net/wasabi/src/http.rs
+++ net/wasabi/src/http.rs
@@ -35,6 +35,36 @@ impl HttpClient {
         }
 
         let socket_addr: SocketAddr = (ips[0], port).into();
+
+        let mut stream = match TcpStream::connect(socket_addr) {
+            Ok(stream) => stream,
+            Err(_) => {
+                return Err(Error::Network(
+                    "Failed to connect to TCP stream".to_string(),
+                ))
+            }
+        };
+
+        let mut request = String::from("Get /");
+        request.push_str(&path);
+        request.push_str(" HTTP/1.1\n");
+
+        // ヘッダの追加
+        request.push_str("HOST: ");
+        request.push_str(&host);
+        request.push('\n');
+        request.push_str("Accept: text/html\n");
+        request.push_str("Connection: close\n");
+        request.push('\n');
+
+        let _bytes_written = match stream.write(requesr.as_bytes()) {
+            Ok(bytes) => bytes,
+            Err(_) => {
+                return Err(Error::Network(
+                    "Failed to send a request to TCP stream".to_string(),
+                ))
+            }
+        };
     }
 }
 
